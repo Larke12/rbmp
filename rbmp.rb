0644b2a32118dd30b04d29816dc8c97815a23ee0
Ryan Hammett <rhammett@ycp.edu>
2016-03-13 12:16:20 -0400

    Ruby can call main window

--- rbmp.rb
+++ rbmp.rb
@@ -12,13 +12,32 @@ require 'gtk3'
 
 begin $notifies=require 'rnotify'; rescue LoadError; end
 
-# Store location of glade file
-builder_file = "rbmp.ui"
+class RBMP_gui
+	def initialize
+		if __FILE__ == $0
+			# Construct the builder instance and load the UI description
+			@builder = Gtk::Builder.new
+			@builder.add_from_file('rbmp.glade')
+			@builder.connect_signals{|handler| method(handler)}
+			Gtk.main()
+		end
+	end
 
-# Construct the builder instance and load the UI description
-builder = Gtk::Builder.new(:file => builder_file)
+	# Connect About Dialog to menu button
+	def on_menu_about_activate
+		p "Now open the window!"
+	end
+	
+	# Play button debug
+	def on_play_butt_clicked
+		p "Play a song!"
+	end
 
-window = builder.get_object("window")
-window.signal_connect("destroy") { Gtk.main_quit }
+	# Self explanatory exit
+	def gtk_main_quit
+		puts "Gtk.main_quit"
+		Gtk.main_quit()
+	end
+end
 
-Gtk.main
+RBMP_gui.new
