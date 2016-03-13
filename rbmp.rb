60ab8a7d9eab03adae3b5cb334553474e700a535
Ryan Hammett <rhammett@ycp.edu>
2016-03-12 22:02:43 -0500

    Updated new builder code

--- rbmp.rb
+++ rbmp.rb
@@ -10,10 +10,15 @@ require 'gtk3'
 #require 'nokogiri'
 #require 'gir_ffi'
 
-#begin $notifies=require 'rnotify'; rescue LoadError; end
+begin $notifies=require 'rnotify'; rescue LoadError; end
 
-builder = Gtk::Builder.new
-builder.add_from_file('rbmp.glade')
-builder.connect_signals {|handler| method(handler) }
+# Store location of glade file
+builder_file = "rbmp.ui"
+
+# Construct the builder instance and load the UI description
+builder = Gtk::Builder.new(:file => builder_file)
+
+window = builder.get_object("window")
+window.signal_connect("destroy") { Gtk.main_quit }
 
 Gtk.main
