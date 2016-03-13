e8d32110a2ef5f7cf0bb95beedbcbd8a555439a6
Ryan Hammett <rhammett@ycp.edu>
2016-03-12 20:13:10 -0500

    Local file cleanup

--- rbmp.rb
+++ rbmp.rb
@@ -1,19 +1,19 @@
 #!/usr/bin/ruby
 
 require 'gtk3'
-require 'find'
-require 'open-uri'
-require 'digest/md5'
-require 'webrick'
-require 'rexml/document'
-require 'yaml'
+#require 'find'
+#require 'open-uri'
+#require 'digest/md5'
+#require 'webrick'
+#require 'rexml/document'
+#require 'yaml'
 #require 'nokogiri'
 #require 'gir_ffi'
 
-begin $notifies=require 'rnotify'; rescue LoadError; end
+#begin $notifies=require 'rnotify'; rescue LoadError; end
 
 builder = Gtk::Builder.new
-builder.add_from_file("rbmp.glade")
+builder.add_from_file('rbmp.glade')
 builder.connect_signals {|handler| method(handler) }
 
 Gtk.main
